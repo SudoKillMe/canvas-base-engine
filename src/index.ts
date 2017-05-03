e4d8db650c794f0e0c933fca16f899784ccbe21f
techer <langcundong@gmail.com>
2017-05-03 23:21:43 +0800

    drag

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { Point } from './point';
 import { window2Canvas, drawReact } from './util';
 /**
  * TODO:
- * 使用离屏CANVAS实现将一个图片放大
- * 实现橡皮筋式选取框
+ * 使用离屏CANVAS实现将一个图片放大 XXX
+ * 实现橡皮筋式选取框  XXX
  * 使用canvas实现对图像的处理，比如滤镜，浮雕效果之类  -- 优先级较低，在完成其他任务之后再完成
+ * 利用canvas实现视频的逐帧播放 -- 优先级较低，在其他人物完成之后再完成
+ * 实现放大镜，利用绘制自身的原理，如果时间充足，再尝试使用离屏canvas实现一遍。
  */
 let range = document.querySelector('#range');
 let text = document.querySelector('#text');
@@ -17,15 +19,20 @@ let offCanvas = document.createElement('canvas');
 let offContext = offCanvas.getContext('2d');
 
 let dragging = false;
-let mousedown; 
+let mousedown;
 let current;
+let offset = { x: 0, y: 0 };
 let originCanvas;
 let choosenImage;
 canvas.width = 1000;
 canvas.height = 500;
 
+originCanvas = context.getImageData(0, 0, canvas.width, canvas.height);
+
 let p = new Polygon(100, 100, 50, 6);
 p.stroke(context);
+
+
 
 let polygon = new Polygon(canvas.width / 2, canvas.height / 2,100,6);
 let image = new Image();
@@ -33,7 +40,7 @@ image.src = '/src/static/images/1.jpg';
 image.onload = function () {
     //context.drawImage(image, 0, 0, canvas.width, canvas.height);
     //offContext.drawImage(image, 0, 0, canvas.width, canvas.height);
-    
+
 }
 
 function storeOriginCanvas() {
@@ -54,10 +61,10 @@ function updateChoosenImage( current ) {
 canvas.addEventListener('mousedown', function(event) {
     console.log('mousedown');
     mousedown = window2Canvas(canvas, event.clientX, event.clientY);
+    offset.x = mousedown.x - p.center_x;
+    offset.y = mousedown.y - p.center_y;
     if ( p.pointInside( mousedown ) ) {
         dragging = true;
-        if ( !originCanvas )
-            storeOriginCanvas();
     }
 });
 
@@ -67,9 +74,23 @@ canvas.addEventListener('mousemove', function(event) {
     if ( originCanvas )
         coverByOriginCanvas();
     current = window2Canvas( canvas, event.clientX, event.clientY );
+    let center_x = current.x - offset.x;
+    let center_y = current.y - offset.y;
+    p.move(center_x, center_y);
+    p.stroke(context);
 });
 
-canvas.addEventListener('mouseup', function(event) {});
+canvas.addEventListener('mouseup', function(event) {
+    console.log('mouseup');
+    dragging = false;
+    if ( originCanvas )
+        coverByOriginCanvas();
+    current = window2Canvas( canvas, event.clientX, event.clientY );
+    let center_x = current.x - offset.x;
+    let center_y = current.y - offset.y;
+    p.move(center_x, center_y);
+    p.stroke(context);
+});
 
 
 //  画图
@@ -101,7 +122,7 @@ canvas.addEventListener('mouseup', function(event) {});
 //     }
 //     console.log(choosenImage);
 //     // excellent
-//     context.drawImage(canvas, mousedown.x, mousedown.y, Math.abs( current.x - mousedown.x ), Math.abs( current.y - mousedown.y ), 0, 0, canvas.width, canvas.height);
+    // context.drawImage(canvas, mousedown.x, mousedown.y, Math.abs( current.x - mousedown.x ), Math.abs( current.y - mousedown.y ), 0, 0, canvas.width, canvas.height);
 // });
 
 // setInterval( function() {
